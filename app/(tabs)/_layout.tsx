2ee6523f3787b609d529f6b4e558e6480eb41a34
= <alex.ser.online@gnail.com>
2024-10-02 18:02:44 +0100

    Added new project

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,19 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
+    <Tabs>
+      <Tabs.Screen name="(home)" options={{
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}/>
+      <Tabs.Screen name="page" options={{
+            title: "Page",
+            headerShown: false,
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name='book' color={color} />,
+            }} />
     </Tabs>
   );
 }
